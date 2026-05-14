ae375b0c3840e1cb1c691bd5197a9714739824c0
kokhee99 <kokhee99@gmail.com>
2026-05-14 10:44:35 +0800

    Update to LED Blinking function

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  pinMode(LED_BUILTIN, OUTPUT);     // initialize the built-in LED pin as an output
+  Serial.begin(9600);            	// initialize serial communication at 9600 bits per second:
+  delay(1000);                      	// wait for serial monitor to open
+ }	
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
+  Serial.print(LED_BUILTIN);             // print the pin number of the LED
+  Serial.println("Pin LED is On");        // print the state of the LED
+  delay(1000);                                    // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // change state of the LED by setting the pin to the LOW voltage level
+  Serial.print(LED_BUILTIN);             // print the pin number of the LED
+  Serial.println("Pin LED is Off");        // print the state of the LED
+  delay(1000);                                    // wait for a second    
 }
